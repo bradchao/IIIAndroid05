8e15a20763f543c2f934c759f84c0247b935cd4e
brad <brad@brad.tw>
2019-11-12 09:50:12 +0800

    2

--- app/src/main/java/tw/org/iii/iiiandroid05/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid05/MainActivity.java
@@ -3,12 +3,24 @@ package tw.org.iii.iiiandroid05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        initListView();
+    }
+
+    private void initListView(){
+        // TODO initListView
+        //adapter = new SimpleAdapter()
     }
 }
